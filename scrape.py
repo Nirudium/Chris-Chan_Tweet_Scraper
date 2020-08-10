c9ed5e3a64c14bdfa25197d4e918ed87c17cd9e4
unknown <alph4nir@riseup.net>
2020-08-09 22:57:49 -0700

    Actually added functionality

--- scrape.py
+++ scrape.py
@@ -1,10 +1,30 @@
 import tweepy 
 from configscrape import __DATA__, CWCUSERID  
-import writetofile
+from copy import deepcopy
+from datetime import datetime
+import csv
 
+
+### Connects to Twitter via a Developer App
 auth = tweepy.OAuthHandler(__DATA__["consumer_key"], __DATA__["consumer_secret"]) 
 auth.set_access_token(__DATA__["access_token"], __DATA__["access_token_secret"]) 
 api = tweepy.API(auth) 
-status = api.get_status(CWCUSERID) 
 
-print(status) 
+### Writes to a csv file (Should be openable in Excel for easy access and management) 
+def writeToArchive(text, dateOfWriting):
+    with open("cwctweets-" + datetime.today().strftime('%Y-%m-%d') + ".csv") as cwcTweets:
+        cwcWriter = csv.writer(cwcTweets, delimiter=' ', quotechar=',', quoting=csv.QUOTE_MINIMAL)
+        cwcWriter.writerow([text] + [dateOfWriting.strftime('%Y-%m-%d')])
+
+### Wrapper function to make it more human readable
+def grabLatestTweet():
+    latestTweet = api.user_timeline(CWCUSERID, count = 1)[0]
+    return latestTweet
+
+### starts up the scraper
+def startScraper():
+    mostRecentTweet = deepcopy(grabLatestTweet())
+    while True:
+        if grabLatestTweet() != mostRecentTweet:
+            mostRecentTweet = deepcopy(grabLatestTweet())
+            writeToArchive(mostRecentTweet.text, mostRecentTweet.created_at)
--- writetofile.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-import csv
-
-def writetomainfile(text, date):
-    with open("cwctweets-" + datetime.today().strftime('%Y-%m-%d') + ".csv") as cwctweets:
-        return
